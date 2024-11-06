ae4bc253d51ec7b24c1c67987705f1da0cadc945
YannSigmaAntiSuki <115077840+yann1945@users.noreply.github.com>
2024-11-05 23:14:40 -0800

    Update liputan6.js

--- berita/liputan6.js
+++ berita/liputan6.js
@@ -44,7 +44,7 @@ module.exports = function(app) {
 
       res.status(200).json({
         status: 200,
-        creator: "Lenwy",
+        creator: "YannXD",
         data: data
       });
     } catch (error) {
